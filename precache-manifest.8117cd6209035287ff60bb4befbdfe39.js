a7c7192f72ea96f45794585d470b0dfd114d1c51
pasha-log <pasha.log@gmail.com>
2023-01-17 11:09:44 -0800

    Updates

---
+++ precache-manifest.8117cd6209035287ff60bb4befbdfe39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16eba66b0656e36e03b6",
+    "url": "/static/css/main.e60d54f6.chunk.css"
+  },
+  {
+    "revision": "16eba66b0656e36e03b6",
+    "url": "/static/js/main.16eba66b.chunk.js"
+  },
+  {
+    "revision": "4155419598f1b242b5e2",
+    "url": "/static/js/1.41554195.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ffbd52262c04e61dc3da1a9c7477e650",
+    "url": "/index.html"
+  }
+];
